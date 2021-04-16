f13f5eb0bb01ae3d5e2e3a1c27853a58a55aabf7
MohammedSayedAdam <30482644+MohammedSayedAdam@users.noreply.github.com>
2021-04-17 01:57:27 +0200

    Atm App in console and windows forms
    this atm repo contains two apps (Console and Windows Forms) with some validations

---
+++ ATM_WinForm/ATM_WinForm/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_btn_Click(object sender, EventArgs e)
+        {
+            string user = User_txtBox.Text;
+            string pass = Password_txtBox.Text;
+            if ((user != "" || user != null) && pass != "" || pass != null)
+            {
+                if (user == "admin" && pass == "1234")
+                {
+                    Operations op = new Operations();
+                    this.Hide();
+                    op.Show();
+                }
+                else
+                    MessageBox.Show("You Should Write a correct User And Password");
+            }
+            else
+                MessageBox.Show("You Should Write User And Password");
+        }
+    }
+}
